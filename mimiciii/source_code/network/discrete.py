6b1f032809c41636128246513c1fe0e1c62dc99b
tooooooot <linyugung@gmail.com>
2023-06-22 23:33:05 +0800

    Add discrete policy network.

---
+++ mimiciii/source_code/network/discrete.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+
+class PolicyMLP(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_size=(128, 128)) -> None:
+        super().__init__()
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.layer_size = [state_dim] + list(hidden_size) + [action_dim]
+        self.main = []
+        for i in range(len(self.layer_size) - 2):
+            self.main.append(nn.Linear(self.layer_size[i], self.layer_size[i + 1]))
+            self.main.append(nn.BatchNorm1d(self.layer_size[i + 1]))
+            self.main.append(nn.LeakyReLU())
+        self.main = nn.Sequential(*self.main)
+        self.output = nn.Linear(self.layer_size[-2], self.layer_size[-1])
+
+
+    def forward(self, x):
+        y = self.main(x)
+        y = self.output(y)
