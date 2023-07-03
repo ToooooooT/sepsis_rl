bc0b406c65c98f4ee16ac0db6ab160e1010e358c
tooooooot <linyugung@gmail.com>
2023-07-03 13:44:17 +0800

    Fix the bug of SAC WIS estimator.

--- mimiciii/source_code/train.py
+++ mimiciii/source_code/train.py
@@ -11,7 +11,9 @@ from collections import defaultdict
 from tqdm import tqdm
 
 from agents import DQN, SAC, BaseAgent
-from utils import Config, plot_action_dist, plot_action_distribution, plot_estimate_value, animation_action_distribution
+from utils import Config, plot_action_dist, plot_action_distribution, plot_estimate_value, \
+                animation_action_distribution, plot_pos_neg_action_dist, plot_diff_action_SOFA_dist, \
+                plot_diff_action, plot_survival_rate
 from network import DuellingMLP, PolicyMLP
 
 from torch.utils.tensorboard import SummaryWriter
@@ -28,7 +30,7 @@ def parse_args():
     parser.add_argument("--reg_lambda", type=int, help="regularization term coeficient", default=5)
     parser.add_argument("--agent", type=str, help="agent type", default="D3QN")
     parser.add_argument("--test_dataset", type=str, help="test dataset", default="test")
-    parser.add_argument("--valid_freq", type=int, help="validation frequency", default=10)
+    parser.add_argument("--valid_freq", type=int, help="validation frequency", default=50)
     parser.add_argument("--target_net_freq", type=int, help="the frequency of updates for the target networks", default=1)
     parser.add_argument("--seed", type=int, help="random seed", default=10)
     args = parser.parse_args()
@@ -122,14 +124,16 @@ def testing(test, model: BaseAgent):
         if isinstance(model, D3QN_Agent):
             model.model.eval()
             actions = model.model(batch_state).max(dim=1)[1].view(-1, 1).cpu().numpy()
+            ret = (actions, None)
         elif isinstance(model, SAC_Agent):
             model.actor.eval()
-            actions, _, _, _ = model.actor.get_action(batch_state)
+            actions, _, _, action_probs = model.actor.get_action(batch_state)
             actions = actions.view(-1, 1).cpu().numpy()
+            ret = (actions, action_probs)
         
     model.save_action(actions)
 
-    return actions
+    return ret
 
 
 def training(model: BaseAgent, valid, config, valid_dataset, id_index_map, args):
@@ -138,14 +142,11 @@ def training(model: BaseAgent, valid, config, valid_dataset, id_index_map, args)
         valid           : batch_state and action (dict)
         valid_dataset   : original valid dataset (DataFrame)
         id_index_map    : indexes of each icustayid (dict)
-    Returns:
-        policy_val  : list of policy estimate value
-        expert_val  : list of expert value
     '''
     writer = SummaryWriter(model.log_dir)
     loss = 0
-    expert_val = list()
-    policy_val = list()
+    avg_expert_returns = list()
+    avg_policy_returns = list()
     hists = list() # save model actions of validation in every episode 
     valid_freq = args.valid_freq
 
@@ -155,53 +156,57 @@ def training(model: BaseAgent, valid, config, valid_dataset, id_index_map, args)
 
         if i % valid_freq == 0:
             model.save()
-            actions = testing(valid, model)
+            actions, action_probs = testing(valid, model)
             if i % 1000 == 0:
                 hists.append(model.action_selections)
-            p_val, e_val = WIS_estimator(actions, valid_dataset, id_index_map)
-            policy_val.append(p_val)
-            expert_val.append(e_val)
+            avg_p_return, avg_e_return, _ = WIS_estimator(actions, action_probs, valid_dataset, id_index_map, args)
+            avg_policy_returns.append(avg_p_return)
+            avg_expert_returns.append(avg_e_return)
 
-            writer.add_scalars('WIS_estimator', dict(zip(['learned', 'expert'], [p_val, e_val])), i)
+            writer.add_scalars('WIS_estimator', dict(zip(['learned', 'expert'], [avg_p_return, avg_e_return])), i)
 
     plot_action_distribution(model.action_selections, model.log_dir)
     animation_action_distribution(hists, model.log_dir)
-    plot_estimate_value(expert_val, policy_val, model.log_dir)
+    plot_estimate_value(avg_expert_returns, avg_policy_returns, model.log_dir)
 
 
-def WIS_estimator(actions, expert_data, id_index_map):
+def WIS_estimator(actions, action_probs, expert_data, id_index_map, args):
     '''
     Args:
     actions         : policy action (tensor)
     expert_data     : original expert dataset (DataFrame)
     id_index_map    : indexes of each icustayid (dict)
     Returns:
-        policy_val  : policy estimation value
-        expert_val  : expert value
+        avg_policy_return: average policy return
+        avg_expert_return: average expert return
+        policy_return: expected return of each patient; expected shape (B,)
     '''
     # compute all trajectory total reward and weight imporatance sampling
     gamma = 0.99
-    policy_val = expert_val = 0
-    weight = 0
-    for id in id_index_map.keys():
+    num = len(id_index_map)
+    policy_return = np.zeros((num,)) 
+    expert_return = np.zeros((num,)) 
+    weights = np.zeros((num,))
+    for i, id in enumerate(id_index_map.keys()):
         start, end = id_index_map[id][0], id_index_map[id][-1]
         reward = 0
         ratio = 1
         for index in range(end, start - 1, -1):
             # assume policy take the max action in probability of 0.99 and any othe actions of 0.01 
-            ratio = ratio * 0.99 if int(actions[index]) == int(expert_data.loc[index, 'action']) else ratio * 0.01
+            if args.agent == 'D3QN':
+                ratio = ratio * 0.99 if int(actions[index]) == int(expert_data.loc[index, 'action']) else ratio * 0.01
+            elif args.agent == 'SAC':
+                ratio *= action_probs[index, int(actions[index])]
             # total reward
             reward = gamma * reward + expert_data.loc[index, 'reward']
 
-        weight += ratio
-        policy_val += ratio * reward
-        expert_val += reward 
+        weights[i] = ratio
+        policy_return[i] = ratio * reward
+        expert_return[i] = reward 
 
-    n = len(id_index_map.keys())
-    weight /= n
-    policy_val = policy_val / n / weight
-    expert_val /= n
-    return policy_val, expert_val
+    avg_policy_return = (policy_return / weights).mean()
+    avg_expert_return = expert_return.mean()
+    return avg_policy_return, avg_expert_return, policy_return
 
 
 if __name__ == '__main__':
@@ -286,15 +291,21 @@ if __name__ == '__main__':
     test, id_index_map = process_dataset(test_dataset)
 
     print('Start testing')
-    actions = testing(test, model)
+    actions, action_probs = testing(test, model)
 
     test_data_unnorm['action'] = test_dataset['action']
     test_data_unnorm['policy action'] = actions
     test_data_unnorm.to_csv(os.path.join(model.log_dir, 'test_data_predict.csv'), index=False)
-    policy_val, expert_val = WIS_estimator(actions, test_dataset, id_index_map)
+    negative_traj = test_data_unnorm.query('died_in_hosp == 1.0 | died_within_48h_of_out_time == 1.0 | mortality_90d == 1.0')
+    positive_traj = test_data_unnorm.query('died_in_hosp != 1.0 & died_within_48h_of_out_time != 1.0 & mortality_90d != 1.0')
+    avg_policy_return, avg_expert_return, policy_return = WIS_estimator(actions, action_probs, test_dataset, id_index_map, args)
     plot_action_dist(actions, test_data_unnorm, model.log_dir)
+    plot_pos_neg_action_dist(positive_traj, negative_traj, log_path)
+    plot_diff_action_SOFA_dist(positive_traj, negative_traj, log_path)
+    plot_diff_action(positive_traj, negative_traj, log_path)
+    plot_survival_rate(policy_return, id_index_map, test_data_unnorm, log_path)
     with open(os.path.join(log_path, 'evaluation.txt'), 'w') as f:
-        f.write(f'policy WIS estimator: {policy_val:.5f}\n')
-        f.write(f'expert: {expert_val:.5f}')
-    print(f'policy WIS estimator: {policy_val:.5f}')
-    print(f'expert: {expert_val:.5f}')
+        f.write(f'policy WIS estimator: {avg_policy_return:.5f}\n')
+        f.write(f'expert: {avg_expert_return:.5f}')
+    print(f'policy WIS estimator: {avg_policy_return:.5f}')
+    print(f'expert: {avg_expert_return:.5f}')
