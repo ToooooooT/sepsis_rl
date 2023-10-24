fix: don't use batch normalization in network

mimiciii/source_code/network/duelling.py
@@ -10,7 +10,7 @@ class DuellingMLP(nn.Module):
         self.main = []
         for i in range(len(self.layer_size) - 1):
             self.main.append(nn.Linear(self.layer_size[i], self.layer_size[i + 1]))
-            self.main.append(nn.BatchNorm1d(self.layer_size[i + 1]))
+            # self.main.append(nn.BatchNorm1d(self.layer_size[i + 1]))
             self.main.append(nn.LeakyReLU())
         self.main = nn.Sequential(*self.main)
         self.adv = nn.Linear(self.layer_size[-1] // 2, self.num_actions)