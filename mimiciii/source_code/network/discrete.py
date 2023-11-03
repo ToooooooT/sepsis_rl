fbf31b27f04d34d0392412a6df11cef431c7426b
ToooooooT <linyugung@gmail.com>
2023-11-03 23:44:23 +0800

    fix: delete the batch normalization layer

--- mimiciii/source_code/network/discrete.py
+++ mimiciii/source_code/network/discrete.py
@@ -9,7 +9,6 @@ class PolicyMLP(nn.Module):
         self.main = []
         for i in range(len(self.layer_size) - 2):
             self.main.append(nn.Linear(self.layer_size[i], self.layer_size[i + 1]))
-            self.main.append(nn.BatchNorm1d(self.layer_size[i + 1]))
             self.main.append(nn.LeakyReLU())
         self.main = nn.Sequential(*self.main)
         self.output = nn.Linear(self.layer_size[-2], self.layer_size[-1])
