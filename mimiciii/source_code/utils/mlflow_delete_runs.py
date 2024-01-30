a54a4b45bdbcd4549216a36b5c2e0fb46c11b406
ToooooooT <linyugung@gmail.com>
2024-01-30 22:28:12 +0800

    fix: modify experiment id to arguments

--- mimiciii/source_code/utils/mlflow_delete_runs.py
+++ mimiciii/source_code/utils/mlflow_delete_runs.py
@@ -1,24 +1,37 @@
 import mlflow
 import shutil
 import os
+from argparse import ArgumentParser
 
-ml_runs_path = '../logs/mlruns'
-ml_artifacts_path = '../logs/mlartifacts'
     
 def get_run_dir(artifacts_uri):
     return artifacts_uri[18:-10]
     
-def remove_run_dir(run_dir):
+def remove_run_dir(run_dir, ml_runs_path, ml_artifacts_path):
     path = os.path.join(ml_runs_path, run_dir)
     shutil.rmtree(path, ignore_errors=True)
     path = os.path.join(ml_artifacts_path, run_dir)
     shutil.rmtree(path, ignore_errors=True)
-    
-experiment_id = 422843293446608405
-# experiment_id = 378651521004183178
-    
-exp = mlflow.tracking.MlflowClient(tracking_uri=ml_runs_path)
-    
-runs = exp.search_runs(str(experiment_id), run_view_type=2)
 
-_ = [remove_run_dir(get_run_dir(run.info.artifact_uri)) for run in runs]
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--experiment_id", type=int, help="experiment id", default=None)
+    return parser.parse_args()
+
+if __name__ == '__main__':
+
+    ml_runs_path = '../logs/mlruns'
+    ml_artifacts_path = '../logs/mlartifacts'
+
+    args = parse_args()
+    
+    experiment_id = args.experiment_id
+
+    if experiment_id == None:
+        print('assign a experiment id')
+        
+    exp = mlflow.tracking.MlflowClient(tracking_uri=ml_runs_path)
+        
+    runs = exp.search_runs(str(experiment_id), run_view_type=2)
+
+    _ = [remove_run_dir(get_run_dir(run.info.artifact_uri), ml_runs_path, ml_artifacts_path) for run in runs]
